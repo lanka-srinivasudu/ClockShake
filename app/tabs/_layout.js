f9f80c61579ec83912de8def4f94fc5f3602579e
Lanka Srinivasudu <lankasrinivasudu8gmail.com@Lankas-Laptop.local>
2025-02-05 00:11:18 +0530

    Initial commit

---
+++ app/tabs/_layout.js
@@ -0,0 +1,38 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          // backgroundColor:"transparent",
+          width: "95%",
+          alignItems: "center",
+          justifyContent: "space-around",
+          marginHorizontal: "2.5%",
+          bottom: 10,
+          height: 60,
+          bottom: 10,
+          marginHorizontal: "2.5%",
+          borderRadius: 35,
+          shadowColor: "#000",
+          overflow: "hidden",
+          position: "absolute",
+          // zIndex: 1000,
+        },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "gray",
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+        tabBarActiveBackgroundColor: "teal",
+      }}
+    >
+
+      <Tab.Screen/>
+    </Tabs>
+  );
+};
+
+export default Layout;
